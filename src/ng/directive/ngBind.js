628c795a008f22512268f17630ca341c8b445706
jankuca <jan@jankuca.com>
2013-09-24 15:35:56 -0700

    fix(ngBindHtmlDirective): use $watch

--- src/ng/directive/ngBind.js
+++ src/ng/directive/ngBind.js
@@ -13,8 +13,8 @@
  * Typically, you don't use `ngBind` directly, but instead you use the double curly markup like
  * `{{ expression }}` which is similar but less verbose.
  *
- * It is preferrable to use `ngBind` instead of `{{ expression }}` when a template is momentarily 
- * displayed by the browser in its raw state before Angular compiles it. Since `ngBind` is an 
+ * It is preferrable to use `ngBind` instead of `{{ expression }}` when a template is momentarily
+ * displayed by the browser in its raw state before Angular compiles it. Since `ngBind` is an
  * element attribute, it makes the bindings invisible to the user while the page is loading.
  *
  * An alternative solution to this problem would be using the
@@ -134,15 +134,15 @@ var ngBindTemplateDirective = ['$interpolate', function($interpolate) {
  * @element ANY
  * @param {expression} ngBindHtml {@link guide/expression Expression} to evaluate.
  */
-var ngBindHtmlDirective = ['$sce', '$parse', function($sce, $parse) {
+var ngBindHtmlDirective = ['$sce', '$parse', '$watch', function($sce, $parse, $watch) {
   return function(scope, element, attr) {
     element.addClass('ng-binding').data('$binding', attr.ngBindHtml);
 
-    var parsed = $parse(attr.ngBindHtml);
+    var desc = $parse.prepareObservable(attr.ngBindHtml);
     function getStringValue() { return (parsed(scope) || '').toString(); }
 
-    scope.$watch(getStringValue, function ngBindHtmlWatchAction(value) {
-      element.html($sce.getTrustedHtml(parsed(scope)) || '');
+    $watch.watchPaths(scope, desc.paths, function ngBindHtmlWatchAction(value) {
+      element.html($sce.getTrustedHtml(desc.get(scope)) || '');
     });
   };
 }];
